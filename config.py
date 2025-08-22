df81963f5ea6994c171ba44e3b3092533dcda08f
luiz3x <luizlifec@gmail.com>
2025-08-22 09:52:04 -0300

    fix: Corrige erro de CSRF com a integração do Flask-WTF

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-muito-forte'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///synapcortex.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://synapcortex_user:KJK41P8ah9hHVVBqxDk1iQcHCXH2x3Qt@dpg-d2hqcfodl3ps73aulbtg-a.oregon-postgres.render.com/synapcortex_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

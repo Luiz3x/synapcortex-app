48df1131d859300a3a5c6fa69f940b12de78f9e4
luiz3x <luizlifec@gmail.com>
2025-08-24 21:29:30 -0300

    feat: Adiciona setup.py para tornar o projeto um pacote instalável

---
+++ setup.py
@@ -0,0 +1,10 @@
+# /setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='synapcortex',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
